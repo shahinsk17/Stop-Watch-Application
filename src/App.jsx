57d7249136305f4adcfa9066872c91c612f2b7d2
shahinsk17 <shahinshaikh6828@gmil.com>
2024-10-03 21:51:18 +0530

    Update description of what you changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let timerRef = useRef(null);
+  const [count, setCount] = useState(0);
+
+  function handleStrt() {
+    timerRef.current = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+  }
+  function handleStop() {
+    clearInterval(timerRef.current);
+    timerRef = null;
+  }
+  function handleReset() {
+    setCount(0);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="stopwatch-container">
+          <h1>Stop Watch</h1>
+          <div className="time-display">{count} Seconds</div>
+
+          <div className="controls">
+            <button className="start-btn" onClick={handleStrt}>
+              Start
+            </button>
+
+            <button className="stop-btn" onClick={handleStop}>
+              Stop
+            </button>
+
+            <button className="reset-btn" onClick={handleReset}>
+              Reset
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
